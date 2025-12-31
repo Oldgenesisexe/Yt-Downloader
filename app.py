900f6b0fc9af09204cb20fd44060af8a983121bb
Oldgenesis.exe <eerorr21@gmail.com>
2025-12-31 13:45:32 +0700

    Implement Flask app for video/audio downloads

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+DOWNLOAD_FOLDER = 'downloads'
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form.get('url')
+    mode = request.form.get('mode')
+
+    ydl_opts = {
+        'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
+    }
+
+    if mode == 'audio':
+        ydl_opts['format'] = 'bestaudio/best'
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    else:
+        ydl_opts['format'] = 'best'
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+        if mode == 'audio':
+            filename = os.path.splitext(filename)[0] + ".mp3"
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+  
